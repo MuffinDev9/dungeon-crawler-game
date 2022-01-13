5b3cfbfb7b42ce4350e833688e04366853a525ea
MuffinDev <92362765+MuffinDev9@users.noreply.github.com>
2022-01-13 09:47:37 +0000

    dassdasdasdsdadsadas

--- main.py
+++ main.py
@@ -8,19 +8,21 @@ my_sprite = sprites.create(assets.image("""player-right"""), SpriteKind.player)
 scene.camera_follow_sprite(my_sprite)
 velocity_x = 100
 velocity_y = 100
-room = "Start"
+buttonpressed = 0
 
 # Moving
 
 def on_left_pressed():
     if not blockMenu.is_menu_open():
         my_sprite.set_image(assets.image("""player-left"""))
+        buttonpressed = 1
         pass
 controller.left.on_event(ControllerButtonEvent.PRESSED, on_left_pressed)
 
 def on_right_pressed():
     if not blockMenu.is_menu_open():
         my_sprite.set_image(assets.image("""player-right"""))
+        buttonpressed = 0
     pass
 controller.right.on_event(ControllerButtonEvent.PRESSED, on_right_pressed)
 
@@ -33,8 +35,23 @@ game.on_update(controlthing)
 def on_a_pressed():
     if blockMenu.is_menu_open():
         menuthings()
-
+    else:
+        if buttonpressed == 0:
+            projectile = sprites.create_projectile_from_sprite(assets.image("""swardright"""), my_sprite, 100, 0)
+            
 controller.A.on_event(ControllerButtonEvent.PRESSED, on_a_pressed)
+
+def on_up_pressed():
+    if not blockMenu.is_menu_open():
+        buttonpressed = 2
+    pass
+controller.up.on_event(ControllerButtonEvent.PRESSED, on_up_pressed)
+
+def on_down_pressed():
+    if not blockMenu.is_menu_open():
+        buttonpressed = 3
+    pass
+controller.up.on_event(ControllerButtonEvent.PRESSED, on_down_pressed)
 
 # Main Functions
 
@@ -51,9 +68,15 @@ def levelsandstuff():
     # For Loops
 
     for value in(tiles.get_tiles_by_type(assets.tile("""DoorTopSpawner"""))):
-        door_topp = sprites.create(assets.image("""Doortoop"""), SpriteKind.enemy)
+        door_topp = sprites.create(assets.image("""Doortoop"""))
         door_topp.z = 2
         tiles.place_on_tile(door_topp, value)
+        pass
+    
+    for value2 in(tiles.get_tiles_by_type(assets.tile("""EnemyHere"""))):
+        enemmy = sprites.create(assets.image("""enemy1"""), SpriteKind.enemy)
+        tiles.place_on_tile(enemmy, value2)
+        enemmy.follow(my_sprite, 30)
         pass
 
     # Starting Dialogue
@@ -71,5 +94,12 @@ def levelsandstuff():
     pause(3000)
     my_sprite.say_text(None)
 
-    
+    info.player1.set_life(3)
 
+# Sprites
+
+def enemyone_overlap(sprite, otherSprite):
+    tiles.place_on_tile(my_sprite, tiles.get_tile_location(23, 37))
+    info.player1.change_life_by(-1)
+    pass
+sprites.on_overlap(SpriteKind.player, SpriteKind.enemy, enemyone_overlap)
--- main.ts
+++ main.ts
@@ -6,18 +6,22 @@ let my_sprite = sprites.create(assets.image`player-right`, SpriteKind.Player)
 scene.cameraFollowSprite(my_sprite)
 let velocity_x = 100
 let velocity_y = 100
-let room = "Start"
+let buttonpressed = 0
 //  Moving
 controller.left.onEvent(ControllerButtonEvent.Pressed, function on_left_pressed() {
+    let buttonpressed: number;
     if (!blockMenu.isMenuOpen()) {
         my_sprite.setImage(assets.image`player-left`)
+        buttonpressed = 1
         
     }
     
 })
 controller.right.onEvent(ControllerButtonEvent.Pressed, function on_right_pressed() {
+    let buttonpressed: number;
     if (!blockMenu.isMenuOpen()) {
         my_sprite.setImage(assets.image`player-right`)
+        buttonpressed = 0
     }
     
     
@@ -30,9 +34,28 @@ game.onUpdate(function controlthing() {
     
 })
 controller.A.onEvent(ControllerButtonEvent.Pressed, function on_a_pressed() {
+    let projectile: Sprite;
     if (blockMenu.isMenuOpen()) {
         menuthings()
+    } else if (buttonpressed == 0) {
+        projectile = sprites.createProjectileFromSprite(assets.image`swardright`, my_sprite, 100, 0)
     }
+    
+})
+controller.up.onEvent(ControllerButtonEvent.Pressed, function on_up_pressed() {
+    let buttonpressed: number;
+    if (!blockMenu.isMenuOpen()) {
+        buttonpressed = 2
+    }
+    
+    
+})
+controller.up.onEvent(ControllerButtonEvent.Pressed, function on_down_pressed() {
+    let buttonpressed: number;
+    if (!blockMenu.isMenuOpen()) {
+        buttonpressed = 3
+    }
+    
     
 })
 //  Main Functions
@@ -49,13 +72,20 @@ function menuthings() {
 
 function levelsandstuff() {
     let door_topp: Sprite;
+    let enemmy: Sprite;
     tiles.setTilemap(assets.tilemap`room`)
     tiles.placeOnTile(my_sprite, tiles.getTileLocation(23, 37))
     //  For Loops
     for (let value of tiles.getTilesByType(assets.tile`DoorTopSpawner`)) {
-        door_topp = sprites.create(assets.image`Doortoop`, SpriteKind.Enemy)
+        door_topp = sprites.create(assets.image`Doortoop`)
         door_topp.z = 2
         tiles.placeOnTile(door_topp, value)
+        
+    }
+    for (let value2 of tiles.getTilesByType(assets.tile`EnemyHere`)) {
+        enemmy = sprites.create(assets.image`enemy1`, SpriteKind.Enemy)
+        tiles.placeOnTile(enemmy, value2)
+        enemmy.follow(my_sprite, 30)
         
     }
     //  Starting Dialogue
@@ -72,5 +102,12 @@ function levelsandstuff() {
     my_sprite.sayText("Let's Go!")
     pause(3000)
     my_sprite.sayText(null)
+    info.player1.setLife(3)
 }
 
+//  Sprites
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Enemy, function enemyone_overlap(sprite: Sprite, otherSprite: Sprite) {
+    tiles.placeOnTile(my_sprite, tiles.getTileLocation(23, 37))
+    info.player1.changeLifeBy(-1)
+    
+})
